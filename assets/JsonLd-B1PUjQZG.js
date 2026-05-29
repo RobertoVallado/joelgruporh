f71808313e6dfef8304552425c98154b8d4268ee
CGC\rvallado <rvallado@ssmic.com>
2026-05-29 13:38:39 -0400

    Updates

---
+++ assets/JsonLd-B1PUjQZG.js
@@ -0,0 +1,1 @@
+import{j as s,H as i}from"./index-k-PSj9SL.js";function r({schema:t}){return s.jsx(i,{children:s.jsx("script",{type:"application/ld+json",children:JSON.stringify(t)})})}export{r as J};
